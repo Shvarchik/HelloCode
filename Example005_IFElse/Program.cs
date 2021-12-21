d2ef1bac7a66b1bd1d8e2ed41cbb25ebadc39330
Inna <shvartz_i@mail.ru>
2021-12-21 03:08:14 +0300

    add Example005 IfElse

---
+++ Example005_IFElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Привет, как тебя зовут?");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!"); 
+}
+
